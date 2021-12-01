eba42197eb502fc0c53936b5106bbc28ac59879c
Nicole Jamiolkowski <njamiolkowski@healthequity.com>
2021-12-01 12:28:40 -0500

    Completed module

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard = 0,
+        Ranked = 1,
+        ESNU = 2,
+        OneToFour = 3,
+        SixPoint = 4
+    }
+}
