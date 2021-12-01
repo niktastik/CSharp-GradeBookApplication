fcbbb42f2756515d376831f8b07ad162e6adb31a
Nicole Jamiolkowski <njamiolkowski@healthequity.com>
2021-12-01 12:47:16 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,32 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var cohortCount = Math.Floor(Students.Count * .2);
+            var orderedGradeList = Students.OrderBy(x => x.AverageGrade);
+            var higherGradeCount = orderedGradeList.Where(x => x.AverageGrade > averageGrade).Count();
+            var gradeLevelsToSubtract = Math.Floor(higherGradeCount / cohortCount);
+
+            switch (gradeLevelsToSubtract)
+            {
+                case 0:
+                    return 'A';
+                case 1:
+                    return 'B';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';
+            }
+            return 'F';
         }
     }
 }
